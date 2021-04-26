f65808ced29cb2810025fb0d87ff9d3506cb44f1
DctorWei1314 <61504793+DctorWei1314@users.noreply.github.com>
2021-04-26 21:53:02 +0800

    docterwei ftp

---
+++ ftpServer/main.cpp
@@ -0,0 +1,13 @@
+#include "ftpserver.h"
+#include <QApplication>
+#include <QMetaType>
+//qRegisterMetaType<PVCI_CAN_OBJ>("PVCI_CAN_OBJ");
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ftpServer w;
+    w.show();
+
+    return a.exec();
+}
